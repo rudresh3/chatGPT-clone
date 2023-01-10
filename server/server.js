c5527dbefc0ef7cefc6cd6b041d7b541e293ddac
rudresh3 <itsrudra96@gmail.com>
2023-01-10 21:44:55 +0530

    serverside changes

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Pretend you are Rudresh ${prompt}`,
+      prompt: `Pretend you are Rudresh and your from chikkamagaluru karnataka and tell them how beautiful chikkamagaluru city is ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
